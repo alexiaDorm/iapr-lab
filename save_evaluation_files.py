047b3ee3fb689e812f231992f192d05d7d969b62
jonsdott <valgerdurj2089@gmail.com>
2023-05-31 21:54:50 +0200

    create environment that works on exam day for test_dataset

--- save_evaluation_files.py
+++ save_evaluation_files.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 def export_solutions(image_index , solutions , path = "data_project"  , group_id = "32"):
     """
-    Wrapper funciton to load image and save solution
+    Wrapper function to load image and save solution
 
     solutions :
         image_index : index of the image to solve
@@ -65,6 +65,8 @@ def export_solutions(image_index , solutions , path = "data_project"  , group_id
     save_feature_map    (image_index, solutions[1], saving_path)
     save_cluster        (image_index, solutions[2], saving_path)
     #save_solved_puzzles (image_index, solutions[3], saving_path)
+
+
 
     return None
 
@@ -130,19 +132,19 @@ def save_solved_puzzles(image_index , solution, saving_path):
 #example of how to use the function
 
 ## random data
-mask = np.random.randint(0,2,(2000,2000))
-feature_map = np.random.rand(30, 200)
-cluster = [[np.zeros((128,128,3)) + np.random.randint(i*100 , (i+1)*80, size=3) for _ in range(np.random.randint(10,20))] for i in range(3)]
+#mask = np.random.randint(0,2,(2000,2000))
+#feature_map = np.random.rand(30, 200)
+#cluster = [[np.zeros((128,128,3)) + np.random.randint(i*100 , (i+1)*80, size=3) for _ in range(np.random.randint(10,20))] for i in range(3)]
 
 ## append the outliers too!
-outliers =[np.zeros((128,128,3)) + np.random.randint(0,254 , size =3 ) for _ in range(3)]
-cluster.append(outliers)
+#outliers =[np.zeros((128,128,3)) + np.random.randint(0,254 , size =3 ) for _ in range(3)]
+#cluster.append(outliers)
 
-solved_puzzle = [np.zeros((128*3,128*4,3)) + np.random.randint(0,254, size=3) for _ in range(3)]
+#solved_puzzle = [np.zeros((128*3,128*4,3)) + np.random.randint(0,254, size=3) for _ in range(3)]
 
-solution = [mask, feature_map, cluster, solved_puzzle]
+#solution = [mask, feature_map, cluster, solved_puzzle]
 
 #saving solution for image 1
-export_solutions(1,  solution, path = "data_project", group_id = "00")
+#export_solutions(1,  solution, path = "data_project", group_id = "00")
 
 
